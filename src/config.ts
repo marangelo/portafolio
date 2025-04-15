2d86d532cfa804b87f34cb0b6ff270c4022fb754
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2025-04-15 12:44:01 -0600

    ✅ [ADD] Youtube Channel

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Personal Portfolio Marangelo';
+export const SITE_TITLE = 'Portfolio Marangelo';
 export const SITE_DESCRIPTION = 'DevOps enthusiast with a passion for Docker, programming, and Kubernetes. Building scalable solutions for seamless deployments';
